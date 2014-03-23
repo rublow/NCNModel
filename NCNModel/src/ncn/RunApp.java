859370802a481af4bb394fdd3c1849e8ef73c412
Marchewa <lucasmarchewka@gmail.com>
2014-03-24 00:17:46 +0100

    Added random methods for Agents. Refactored counting

--- NCNModel/src/ncn/RunApp.java
+++ NCNModel/src/ncn/RunApp.java
@@ -1,6 +1,11 @@
 package ncn;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import ncn.Agent.Chars;
@@ -10,158 +15,116 @@ import ncn.Helper.FactorsCounting;
 
 public class RunApp {
 
-//  static class Dupa extends Thread implements Runnable {
-//
-//    @Override
-//    public void run() {
-//      // TODO Auto-generated method stub
-//      for (int i = 0; i < 10; ++i) {
-//        System.out.println("Dupa: " + i + " " + getId());
-//        try {
-//          Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//          // TODO Auto-generated catch block
-//          e.printStackTrace();
-//        }
-//      }
-//    }
-//  }
-//
-//  private static Thread [] t = new Thread[5];
+  public static void main(String args[]) throws InterruptedException, IOException {
 
-  public static void main(String args[]) throws InterruptedException {
-//    t[0] = new Thread(new Dupa());
-//    t[1] = new Thread(new Dupa());
-//    t[2] = new Thread(new Dupa());
-//    t[3] = new Thread(new Dupa());
-//    t[4] = new Thread(new Dupa());
-//
-//    for(Thread y : t) {
-//      y.start();
-//    }
-//    
-//    for (int i = 0; i < 5; ++i) {
-//     Thread.sleep(500);
-//      System.out.println("main");
-//    }
-//    
-//    for(Thread y : t) {
-//      y.join();
-//    }
-//
-//    System.out.println("end");
-//  }
-//}
+    Agent a1 = Agent.createAgent();
 
- Agent a1 = Agent.createAgent();
+    Agent a2 =
+        Agent.createAgent("Zenek", Gender.FEMALE, "POLISH", 5, Education.Economic,
+            Chars.Experienced, Chars.ForeignStudies, Chars.noPostGradStud, Chars.noMBA,
+            Chars.notChairman);
 
- Agent a2 =
- Agent.createAgent("Zenek", Gender.FEMALE, "POLISH", 5, Education.Economic,
- Chars.Experienced, Chars.ForeignStudies, Chars.noPostGradStud, Chars.noMBA,
- Chars.notChairman);
+    Agent a3 =
+        Agent.createAgent("Janek", Gender.FEMALE, "ENGLISH", 3, Education.Technical,
+            Chars.inExperienced, Chars.noForeignStudies, Chars.PostGradStud, Chars.MBA,
+            Chars.notChairman);
 
- Agent a3 =
- Agent.createAgent("Janek", Gender.FEMALE, "ENGLISH", 3, Education.Technical,
- Chars.inExperienced, Chars.noForeignStudies, Chars.PostGradStud, Chars.MBA,
- Chars.notChairman);
+    Agent a4 =
+        Agent.createAgent("Jozek", Gender.MALE, "TURKISH", 6, Education.Law, Chars.inExperienced,
+            Chars.noForeignStudies, Chars.noPostGradStud, Chars.MBA, Chars.notChairman);
 
- Agent a4 =
- Agent.createAgent("Jozek", Gender.MALE, "TURKISH", 6, Education.Law, Chars.inExperienced,
- Chars.noForeignStudies, Chars.noPostGradStud, Chars.MBA, Chars.notChairman);
+    Agent a5 =
+        Agent.createAgent("Zdzich", Gender.MALE, "POLISH", 7, Education.Other, Chars.Experienced,
+            Chars.ForeignStudies, Chars.noPostGradStud, Chars.MBA, Chars.notChairman);
 
- Agent a5 =
- Agent.createAgent("Zdzich", Gender.MALE, "POLISH", 7, Education.Other, Chars.Experienced,
- Chars.ForeignStudies, Chars.noPostGradStud, Chars.MBA, Chars.notChairman);
+    Agent a6 =
+        Agent.createAgent("Romek", Gender.MALE, "POLISH", 2, Education.Economic, Chars.Experienced,
+            Chars.ForeignStudies, Chars.PostGradStud, Chars.MBA, Chars.notChairman);
 
- Agent a6 =
- Agent.createAgent("Romek", Gender.MALE, "POLISH", 2, Education.Economic, Chars.Experienced,
- Chars.ForeignStudies, Chars.PostGradStud, Chars.MBA, Chars.notChairman);
+    Agent a7 =
+        Agent.createAgent("Franek", Gender.MALE, "POLISH", 9, Education.Technical,
+            Chars.Experienced, Chars.ForeignStudies, Chars.noPostGradStud, Chars.MBA,
+            Chars.notChairman);
 
- Agent a7 =
- Agent.createAgent("Franek", Gender.MALE, "POLISH", 9, Education.Technical,
- Chars.Experienced, Chars.ForeignStudies, Chars.noPostGradStud, Chars.MBA,
- Chars.notChairman);
+    List<Agent> lista = Arrays.asList(a1, a2, a3, a4, a5, a6, a7);
 
- List<Agent> lista = Arrays.asList(a1, a2, a3, a4, a5, a6, a7);
+    List<Agent> ag = Agent.generateNumberOfAgents(10);
+    for (Agent agentos : ag) System.err.println(agentos.toString());
+    
+    SupervisoryBoard board = SupervisoryBoard.createSupervisoryBoard(Agent.generateNumberOfAgents(10));
 
- SupervisoryBoard board = SupervisoryBoard.createSupervisoryBoard(lista);
+    double deltaCDL = board.getCognitiveDissonanceProbability();
+    int deltaCD = board.getCognitiveDissonance();
+    double deltaRCL = board.getRelationshipConflictProbability();
+    int deltaRC = board.getRelationshipConflict();
 
- double deltaCDL = board.getCognitiveDissonanceProbability();
- int deltaCD = board.getCognitiveDissonance();
- double deltaRCL = board.getRelationshipConflictProbability();
- int deltaRC = board.getRelationshipConflict();
+    Double[] a = {deltaCDL, (double) deltaCD, deltaRCL, (double) deltaRC};
+    String initial =
+        new String("Initial CognitiveDissonanceProbability " + deltaCDL + "\r\n"
+            + "Initial CognitiveDissonance " + deltaCD + "\r\n"
+            + "Initial RelationshipConflictProbability " + deltaRCL + "\r\n"
+            + "Initial RelationshipConflict " + deltaRC + "\r\n");
 
- // Thread thread1 = new Thread(Meeting);
+    Helper.toFile(initial, true);
 
- System.err.println("Before CognitiveDissonanceProbability " + deltaCDL);
- System.err.println("Before CognitiveDissonance " + deltaCD);
+    Meeting m1 = new Meeting(0, 0);
 
- System.err.println("Before RelationshipConflictProbability " + deltaRCL);
- System.err.println("Before RelationshipConflict " + deltaRC);
+    m1.setNumOfMeetings(Helper.randInt(4, 15));
+
+    for (int j = 0; j < m1.getNumOfMeetings(); ++j) {
+      m1.setNumOfInteractions(Helper.randInt(10, 100));
+
+      for (int i = 0; i < m1.getNumOfInteractions(); ++i) {
+        board.shuffleBoard();
+        Meeting.setCognitiveDissonanceInInteraction(board);
+        Meeting.setRelationshipConflictInInteraction(board);
+      }
+
+      // FactorsCounting fc = new FactorsCounting(board, m1.getNumOfInteractions());
+      // FactorsCounting fc = new FactorsCounting(board, 5);
+      // fc.startBigTask();
 
 
- Meeting m1 = new Meeting(0, 0);
+      String preResult =
+          new String("\r\n\r\nNUM OF ITERACTION BEFORE " + m1.getNumOfInteractions()
+              + "\r\nCognitiveDissonanceProbability " + board.getCognitiveDissonanceProbability()
+              + "\r\nRelationshipConflictProbability " + board.getRelationshipConflictProbability());
 
- m1.setNumOfMeetings(Helper.randInt(4, 15));
+      Helper.toFile(preResult, false);
 
- for (int j = 0; j < m1.getNumOfMeetings(); ++j) {
+      board.setCognitiveDissonanceProbability(board.getCognitiveDissonance(),
+          m1.getNumOfInteractions());
+      board.setRelationshipConflictProbability(board.getRelationshipConflict(),
+          m1.getNumOfInteractions());
 
+      String corrected =
+          new String("\r\n\r\nCORRECTED CognitiveDissonanceProbability "
+              + board.getCognitiveDissonanceProbability()
+              + "\r\n\r\nCORRECTED RelationshipConflictProbability "
+              + board.getRelationshipConflictProbability() + "\r\n\r\nNUM OF ITERACTION AFTER "
+              + m1.getNumOfInteractions());
 
- m1.setNumOfInteractions(Helper.randInt(10, 100));
+      Helper.toFile(corrected, false);
 
- // for (int i = 0; i < m1.getNumOfInteractions(); ++i) {
- // Meeting.setCognitiveDissonanceInInteraction(board);
- // Meeting.setRelationshipConflictInInteraction(board);
- // }
+      DecimalFormat df = new DecimalFormat("0.00");
 
-// FactorsCounting fc = new FactorsCounting(board, m1.getNumOfInteractions());
- FactorsCounting fc = new FactorsCounting(board, 5);
- fc.startBigTask();
- 
- 
-// System.err.println("NUM OF ITERACTION BEFORE " + m1.getNumOfInteractions());
+      double afterCDL = board.getCognitiveDissonanceProbability();
+      int afterCD = board.getCognitiveDissonance();
+      double afterRCL = board.getRelationshipConflictProbability();
+      int afterRC = board.getRelationshipConflict();
 
- System.err.println("CognitiveDissonanceProbability "
- + board.getCognitiveDissonanceProbability());
- System.err.println("RelationshipConflictProbability "
- + board.getRelationshipConflictProbability());
+      String result =
+          new String("\r\n====After " + m1.getNumOfInteractions() + " iteractions ======\r\n"
+              + "====After " + m1.getNumOfMeetings() + " meetings ======\r\n\r\n"
+              + "CognitiveDissonanceProbability " + df.format(afterCDL)
+              + "\r\nRelationshipConflictProbability " + df.format(afterRCL)
+              + "\r\n\r\nCognitiveDissonance " + afterCD + "\r\nRelationshipConflict " + afterRC);
 
- board.setCognitiveDissonanceProbability(board.getCognitiveDissonance(),
- m1.getNumOfInteractions());
- board.setRelationshipConflictProbability(board.getRelationshipConflict(),
- m1.getNumOfInteractions());
+       result += ((afterCD - deltaCD != 0) ? "\r\n\r\nCognitiveDissonance occured" : "\r\nCognitiveDissonance did not occured");
+       result += ((afterRC - deltaRC != 0) ? "\r\nRelationshipConflict occured" : "\r\nRelationshipConflict did not occured");
+       Helper.toFile(result, false);
 
- System.err.println("CORRECTED CognitiveDissonanceProbability "
- + board.getCognitiveDissonanceProbability());
- System.err.println("CORRECTED RelationshipConflictProbability "
- + board.getRelationshipConflictProbability());
+    }
 
-// System.err.println("NUM OF ITERACTION AFTER " + m1.getNumOfInteractions());
-
- double afterCDL = board.getCognitiveDissonanceProbability();
- int afterCD = board.getCognitiveDissonance();
- double afterRCL = board.getRelationshipConflictProbability();
- int afterRC = board.getRelationshipConflict();
-
-// System.err.println("====After " + m1.getNumOfInteractions() + " iteractions ======\n");
-// System.err.println("====After " + m1.getNumOfMeetings() + " meetings ======\n");
- System.err.println(" CognitiveDissonanceProbability " + afterCDL
- + "\nRelationshipConflictProbability " + afterRCL);
- System.err.println(" CognitiveDissonance " + afterCD + "\nRelationshipConflict " + afterRC);
-
- if (afterCD - deltaCD != 0) {
- System.err.println("CognitiveDissonance occured ");
- } else {
- System.err.println("CognitiveDissonance did not occured  ");
- }
-
- if (afterRC - deltaRC != 0) {
- System.err.println("RelationshipConflict occured ");
- } else {
- System.err.println("RelationshipConflict did not occured  ");
- }
-
- }
-
- }
- }
+  }
+}
